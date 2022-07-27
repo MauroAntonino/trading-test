1903a756dde7ab1f5b5d8e5594ae6e869e66a2d6
MauroAntonino <mauro_antonino@yahoo.com.br>
2022-07-27 07:26:04 -0300

    url

--- src/app/application/asgi.py
+++ src/app/application/asgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.asgi import get_asgi_application
 from django.core.handlers.asgi import ASGIHandler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.tokenservice.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.application.settings')
 
 application: ASGIHandler = get_asgi_application()
--- src/app/application/wsgi.py
+++ src/app/application/wsgi.py
@@ -2,6 +2,6 @@ import os
 from django.core.handlers.wsgi import WSGIHandler
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.tokenservice.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.application.settings')
 
 application: WSGIHandler = get_wsgi_application()
