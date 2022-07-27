b4ae2ce99d1ef270727e6e4084d6790fea8e9302
MauroAntonino <mauro_antonino@yahoo.com.br>
2022-07-27 08:35:49 -0300

    update

--- src/app/service/settings.py
+++ src/app/service/settings.py
@@ -31,7 +31,7 @@ MIDDLEWARE: list = [
 
 CORS_ALLOW_ALL_ORIGINS: bool = True
 
-ROOT_URLCONF: str= 'app.urls'
+ROOT_URLCONF: str= 'app.service.urls'
 
 TEMPLATES: list = [
   {
@@ -49,7 +49,7 @@ TEMPLATES: list = [
   },
 ]
 
-WSGI_APPLICATION: str = 'app.wsgi.application'
+WSGI_APPLICATION: str = 'app.service.wsgi.application'
 
 # DATABASES: dict = {
 #   'default': {
